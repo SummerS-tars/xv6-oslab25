0138445f32984ede147c0a2eebbcd13069bdf5ed
thesumst <2240765672@qq.com>
2025-10-09 15:46:12 +0800

    feat: update the implementation of the sleep program.

--- user/sleep.c
+++ user/sleep.c
@@ -1,1 +1,24 @@
-please change it!
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2)
+    {
+        fprintf(2, "sleep: missing operand\n");
+        exit(1);
+    }
+
+    int sleep_time;
+    sleep_time = atoi(argv[1]);
+    if(sleep_time < 0)
+    {
+        fprintf(2, "sleep: invalid time\n");
+        exit(1);
+    }
+
+    sleep(sleep_time);
+    fprintf(1, "(nothing happens for a little while)\n");
+    exit(0);
+}
